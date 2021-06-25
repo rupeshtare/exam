2d229139a7f86d743450d7c70271f8f947426d49
Rupesh Tare <rupesh.tare@emtecinc.com>
2021-06-25 09:15:02 +0530

    New UI for exam

--- client/components/examNew/ExamPaperNewForm.js
+++ client/components/examNew/ExamPaperNewForm.js
@@ -27,20 +27,23 @@ class ExamPaperNewForm extends React.Component {
     return (
       <div className="row p-4 h-70">
         <div className="col-md-8">
-          <div>Question</div>
+          <div className="h-75">Question</div>
           <div className="row">
             <button className="btn btn-success btn-sm">Save & Next</button>
             <button className="btn btn-warning btn-sm">
               Save & Mark for Review
             </button>
-            <button className="btn btn-secondary btn-sm">Clear Response</button>
             <button className="btn btn-primary btn-sm">
               Mark for Review & Next
             </button>
+            <button className="btn btn-light btn-sm"> Next</button>
+            <button className="btn btn-secondary btn-sm">Clear Response</button>
           </div>
         </div>
         <div className="col-md-4">
-          <div className="p-2">Timer</div>
+          <div className="row my-2 py-2 border">
+            <span>Timer</span>
+          </div>
           <QuestionList questionCounts={5} />
         </div>
       </div>
--- client/components/examNew/QuestionList/QuestionList.js
+++ client/components/examNew/QuestionList/QuestionList.js
@@ -4,26 +4,19 @@ import PropTypes from "prop-types";
 const QuestionList = ({ questionCounts }) => {
   return (
     <div className="my-2">
-      <div className="row">
-        <h6>Question List</h6>
-      </div>
-      <div className="row">
+      <div className="row my-2 py-2 border">
         <small className="col-6">
           <span class="badge badge-secondary m-1">0</span>Not Visited
         </small>
         <small className="col-6">
           <span class="badge badge-danger m-1">0</span>Not Answered
         </small>
-      </div>
-      <div className="row">
         <small className="col-6">
           <span class="badge badge-success m-1">0</span>Answered
         </small>
         <small className="col-6">
           <span class="badge badge-primary m-1">0</span>Marked for Review
         </small>
-      </div>
-      <div className="row">
         <small className="col-12">
           <span class="badge badge-primary m-1">
             0 <div className="small-badge">&nbsp;</div>
@@ -31,9 +24,14 @@ const QuestionList = ({ questionCounts }) => {
           Answered & Marked for Review (will be considered for evaluation)
         </small>
       </div>
-      <div className="row my-2">
-        <span class="badge badge-secondary m-1">0</span>
-        <span class="badge badge-secondary m-1">2</span>
+      <div className="row my-2 py-2 border">
+        <div className="col-12 my-1">
+          <h6>Choose a Question</h6>
+        </div>
+        <div className="col-12">
+          <span class="badge badge-secondary m-1">0</span>
+          <span class="badge badge-secondary m-1">2</span>
+        </div>
       </div>
     </div>
   );
